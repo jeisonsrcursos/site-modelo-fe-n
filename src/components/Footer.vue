0a5e65603afe4487ee491516a8c4067f8c8caa2e
Jeison SR <jeison.sr.cursos@gmail.com>
2024-11-18 21:07:00 -0300

    Finalização do layout padrão.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,10 +2,16 @@
 </script>
 
 <template>
-    <footer class="fixed-bottom">
-        <h3 style="text-align: center;">2024&copy;</h3>
+    <footer class="fixed-bottom bg-secondary">
+        <h3 class="titulo-footer">Desenvolvido por [SEU NOME] 2024&copy;</h3>
     </footer>
 </template>
 
 <style scoped>
+    .titulo-footer {
+        text-align: center;
+        color: white;
+        font-size: 18px;
+        padding: 5px 5px;
+    }
 </style>
