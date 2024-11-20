ec95b5b2ac552b4c8efb2555b47be1ef4823db48
Jeison SR <jeison.sr.cursos@gmail.com>
2024-11-19 21:59:14 -0300

    Finalização da estilização da home.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,12 +3,30 @@
 
 <template>
     <section>
-        <h1>Bem Vindo!!!</h1>
+        <div class="image">
+            <img src="../assets/img/paisagens/paisagem-1.jpg" alt="Paisagem 1">
+        </div>
     </section>
 </template>
 
 <style scoped>
+
     h1 {
         text-align: center;
     }
+
+    .image {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .image img {
+        width: 500px;
+        height: auto;
+        margin: 1rem;
+        border: 2px solid gray;
+        box-shadow: 4px 4px 8px black, -4px -4px 8px black;
+    }
+
 </style>
